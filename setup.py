9aa5c6b8c52d1cab8ffc1e39e2b3e1721f13962c
Ben Lee <ben86lee@gmail.com>
2011-07-03 12:37:47 -0400

    added setup.py...

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+ 
+setup(
+    name='browsertools',
+    version='0.1',
+    description='This browser library is designed to make scraping easier and less obvious...ok probably not.',
+    author='ben86lee',
+    url='https://github.com/iiilx/browsertools',
+    package_dir={'': 'src'},
+    py_modules=[
+        'browsertools',
+    ],
+)
